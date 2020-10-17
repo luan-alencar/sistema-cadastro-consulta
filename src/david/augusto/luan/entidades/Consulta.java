9cbb3b5690312d80ba78103af8afbac51e49edbf
Luan-Augusto <luanalencar134@gmail.com>
2020-10-17 18:35:14 -0300

    adicionando classe Consulta e ServicoConsulta

---
+++ src/david/augusto/luan/entidades/Consulta.java
@@ -0,0 +1,52 @@
+package david.augusto.luan.entidades;
+
+import java.util.Date;
+
+public class Consulta {
+	private Paciente paciente;
+	private Medico medico;
+	private Date dataConsulta;
+	private Date dataRetorno;
+
+	public Consulta(Paciente paciente, Medico medico, Date dataConsulta, Date dataRetorno) {
+		this.paciente = paciente;
+		this.medico = medico;
+		this.dataConsulta = dataConsulta;
+		this.dataRetorno = dataRetorno;
+	}
+
+	public Paciente getPaciente() {
+		return paciente;
+	}
+
+	public void setPaciente(Paciente paciente) {
+		this.paciente = paciente;
+	}
+
+	public Medico getMedico() {
+		return medico;
+	}
+
+	public void setMedico(Medico medico) {
+		this.medico = medico;
+	}
+
+	public Date getDataConsulta() {
+		return dataConsulta;
+	}
+
+	public void setDataConsulta(Date dataConsulta) {
+		this.dataConsulta = dataConsulta;
+	}
+
+	public Date getDataRetorno() {
+		return dataRetorno;
+	}
+
+	public void setDataRetorno(Date dataRetorno) {
+		this.dataRetorno = dataRetorno;
+	}
+	
+	
+
+}
---
+++ src/david/augusto/luan/service/ServicoConsulta.java
@@ -0,0 +1,12 @@
+package david.augusto.luan.service;
+
+import static david.augusto.luan.utils.DataUtils.adicionarDias;
+
+import java.util.Date;
+
+import david.augusto.luan.entidades.*;
+
+public class ServicoConsulta {
+	
+		
+}
